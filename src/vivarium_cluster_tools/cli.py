c54d569bba8fd58e16a953c3bc271cf5e29f46c1
Cody <chorst@uw.edu>
2019-02-27 12:38:45 -0800

    add a word to restart doc

--- src/vivarium_cluster_tools/cli.py
+++ src/vivarium_cluster_tools/cli.py
@@ -43,8 +43,8 @@ def run(simulation_configuration, branch_configuration, result_directory, projec
               help='The estimated maximum memory usage of an individual simulate job. The simulations will be run '
                    'with this as a limit.')
 def restart(results_root, project, peak_memory):
-    """Restart a simulation defined by a results directory RESULTS_ROOT. Restarting will not erase existing results,
-    but will start workers to perform the remaining simulations.  RESULTS_ROOT is expected to be an output directory
-    from a previous `psimulate run`."""
+    """Restart a parallel simulation defined by a results directory RESULTS_ROOT. Restarting will not erase existing
+    results, but will start workers to perform the remaining simulations.  RESULTS_ROOT is expected to be an output
+    directory from a previous `psimulate run`."""
 
     main(None, None, results_root, project, peak_memory, restart=True)
