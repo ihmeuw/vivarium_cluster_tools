3e6fb616ee66cd1663e46616202d73c181e7833c
James Collins <collijk@uw.edu>
2019-03-19 12:52:35 -0700

    Update for release

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,3 +1,7 @@
+**1.0.2 - 03/19/19**
+
+ - Hack around hard to reproduce drmaa error.
+
 **1.0.1 - 03/15/19**
 
  - Add additional project options.
--- src/vivarium_cluster_tools/__about__.py
+++ src/vivarium_cluster_tools/__about__.py
@@ -6,7 +6,7 @@ __title__ = "vivarium_cluster_tools"
 __summary__ = "A set of tools for running simulation using vivarium on cluster."
 __uri__ = "https://stash.ihme.washington.edu/projects/CSTE/repos/vivarium_cluster_tools"
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 __author__ = "The vivarium developers"
 __email__ = "vivarium.dev@gmail.com"
