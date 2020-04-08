cff51ee2c53887754f348390cc9b698ea4ded443
halfhorst <chorst22@gmail.com>
2020-04-08 16:26:54 -0700

    bugfix: import drmaa after setting so env var
    After setting the correct environment variable we need to actually
    re-import the drmaa library

--- src/vivarium_cluster_tools/psimulate/utilities.py
+++ src/vivarium_cluster_tools/psimulate/utilities.py
@@ -21,6 +21,7 @@ def get_drmaa() -> object:
     except (RuntimeError, OSError):
         if 'SGE_CLUSTER_NAME' in os.environ:
             os.environ['DRMAA_LIBRARY_PATH'] = '/opt/sge/lib/lx-amd64/libdrmaa.so'
+            import drmaa
         else:
             drmaa = object()
     return drmaa
