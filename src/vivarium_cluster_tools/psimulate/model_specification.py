c87cd74139d3dc050bd5e1c39ba881f11da77c33
James Collins <collijk@uw.edu>
2022-02-16 21:25:50 -0800

    Ditch the RunContext entirely

---
+++ src/vivarium_cluster_tools/psimulate/model_specification.py
@@ -0,0 +1,60 @@
+"""
+==============================
+Model specification management
+==============================
+
+"""
+from pathlib import Path
+from typing import Optional
+
+import yaml
+from vivarium.framework.artifact import parse_artifact_path_config
+from vivarium.framework.configuration import (
+    ConfigTree,
+    ConfigurationError,
+    build_model_specification,
+)
+
+from vivarium_cluster_tools.psimulate import globals as vct_globals
+from vivarium_cluster_tools.psimulate.branches import Keyspace
+
+
+def parse(
+    input_model_specification_path: Optional[Path],
+    artifact_path: Optional[Path],
+    model_specification_path: Path,
+    restart: bool,
+    keyspace: Keyspace,
+) -> ConfigTree:
+    if restart:
+        return build_model_specification(model_specification_path)
+
+    model_specification = build_model_specification(input_model_specification_path)
+
+    artifact_path_is_cli_arg = artifact_path.is_file()
+    artifact_path_in_keyspace = vct_globals.FULL_ARTIFACT_PATH_KEY in keyspace
+
+    if artifact_path_is_cli_arg and artifact_path_in_keyspace:
+        raise ConfigurationError(
+            "Artifact path cannot be specified both in the branch specification file"
+            " and as a command line argument.",
+            str(artifact_path),
+        )
+    elif artifact_path_is_cli_arg:
+        model_specification.configuration[vct_globals.INPUT_DATA_KEY].update(
+            {vct_globals.ARTIFACT_PATH_KEY: str(artifact_path)}, source=__file__
+        )
+    else:
+        # Artifact path comes from the model spec.
+        # Parsing here ensures the key exists and the value points
+        # to an actual file.
+        parse_artifact_path_config(model_specification.configuration)
+
+    return model_specification
+
+
+def persist(
+    model_specification: ConfigTree,
+    model_specification_path: Path,
+) -> None:
+    model_specification_path.write_text(yaml.dump(model_specification.to_dict()))
