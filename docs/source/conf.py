645901c972c34bf3ddd9573660e463f6905bde84
Matthew Kappel <mkappel@uw.edu>
2023-06-26 14:47:44 -0700

    debug print tiem

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,10 +29,10 @@ with (base_dir / "__about__.py").open() as f:
     exec(f.read(), about)
 
 _version = {}
+print(f"base_dir.parent.parent {base_dir.parent.parent}")
+print(f"base_dir.parent {base_dir.parent}")
 try:
     with (base_dir.parent.parent / "_version.py").open() as f:
-        print(f"base_dir.parent.parent {base_dir.parent.parent}")
-        print(f"base_dir.parent {base_dir.parent}")
         exec(f.read(), _version)
 except FileNotFoundError:
     raise UserWarning("To make docs, install via setup the official package or from source.")
