19565dfcca63c4f82da393922b18c7119e3a9f1a
Patrick Nast <pnast@uw.edu>
2023-10-02 14:44:09 -0700

    lint

--- tests/psimulate/worker/test_vivarium_work_horse.py
+++ tests/psimulate/worker/test_vivarium_work_horse.py
@@ -3,7 +3,10 @@ import pytest
 from time import time
 from vivarium.framework.utilities import collapse_nested_dict
 from vivarium_cluster_tools.psimulate.jobs import JobParameters
-from vivarium_cluster_tools.psimulate.worker.vivarium_work_horse import setup_sim, parameter_update_format
+from vivarium_cluster_tools.psimulate.worker.vivarium_work_horse import (
+    setup_sim,
+    parameter_update_format,
+)
 
 
 def test_setup_sim(mocker):
@@ -26,22 +29,19 @@ def test_setup_sim(mocker):
         results_path="~/tmp",
         extras={},
     )
-    
+
     initial_job_params = job_parameters.branch_configuration.copy()
     update_dict = parameter_update_format(job_parameters)
     sim_config = setup_sim(job_parameters).configuration.to_dict()
     job_config = job_parameters.branch_configuration
-    
+
     def compare_dicts(test_dict, ref_dict):
         for k, v in ref_dict.items():
             if isinstance(v, dict):
                 compare_dicts(test_dict[k], v)
             else:
                 assert test_dict[k] == v
-    
+
     for test_dict in [sim_config, job_config]:
         for ref_dict in [initial_job_params, update_dict]:
             compare_dicts(test_dict, ref_dict)
-
-    
-    
