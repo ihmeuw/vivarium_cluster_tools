dcd80f6836be6da103ba88c945362f51efe0e266
Patrick Nast <pnast@uw.edu>
2023-10-05 11:09:03 -0700

    add test for job allocation

--- tests/psimulate/redis_dbs/test_registry.py
+++ tests/psimulate/redis_dbs/test_registry.py
@@ -1,19 +1,28 @@
 import pytest
-import numpy
+import numpy as np
 from vivarium_cluster_tools.psimulate.redis_dbs.registry import RegistryManager
 
 
-@pytest.mark.parametrize("num_queues", [1, 10, 100, 1000], "num_jobs", [1, 10, 100, 1000])
+@pytest.mark.parametrize("num_queues", [1, 5, 7, 10, 25, 50])
+@pytest.mark.parametrize("num_jobs", [1, 10, 1000, 2005, 10000, 20000])
 def test_allocate_jobs(mocker, num_queues, num_jobs):
-    num_queues = 10
     # mock the QueueManager class
-    mocker.patch('registry.QueueManager')
-    manager = RegistryManager(num_queues,0)
+    mocker.patch('vivarium_cluster_tools.psimulate.redis_dbs.registry.QueueManager')
+    queues = [(f'queue_{i}', i) for i in range(num_queues)]
+    manager = RegistryManager(queues,0)
     # create a list of dummy jobs based on a list of 100 integers
     jobs = [{'job': i} for i in range(num_jobs)]
-    # make a 2x2 numpy array of all the jobs in every queue
-    queued_jobs = [manager.allocate_jobs(jobs) for _ in range(num_queues)]
-    # Make sure that jobs get allocated across queues in a round-robin fashion
-    assert numpy.array(queued_jobs).T.flatten() == range(num_jobs)
+    # make a numpy array of all the jobs in every queue
+    jobs_by_queue = list(manager.allocate_jobs(jobs))
+    test_jobs = []
+    # Simulate the order that a single worker would pick up jobs round-robin
+    while jobs_by_queue:
+        for i, queue in enumerate(jobs_by_queue):
+            if not queue:
+                jobs_by_queue.pop(i)
+            else:
+                test_jobs.append(queue.pop(0))
+    assert test_jobs == jobs
+    
     
     
