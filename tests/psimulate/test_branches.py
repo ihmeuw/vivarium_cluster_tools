8f594073441765741423da68f13a90a2e637fb19
patricktnast <130876799+patricktnast@users.noreply.github.com>
2023-10-09 09:41:38 -0700

    Prioritize jobs by filling up scenarios, then seeds, then draws (#192)
    * allocate jobs in round robin
    
    * remove now unused_chunks fn
    
    * change the function a bit
    
    * add test of job order
    
    * skeleton of test for job to queue
    
    * add test for job allocation
    
    * adjust order
    
    * revert unintended change
    
    * re-revert unintended change
    
    * Update registry.py
    
    * Update test_registry.py
    
    * lint
    
    * remove numpy
    
    * improve tests
    
    * lint
    
    * Update test_branches.py
    
    remove itertools

--- tests/psimulate/test_branches.py
+++ tests/psimulate/test_branches.py
@@ -2,6 +2,7 @@ import pytest
 from vivarium.framework.utilities import collapse_nested_dict
 
 from vivarium_cluster_tools.psimulate.branches import (
+    Keyspace,
     calculate_random_seeds,
     expand_branch_templates,
 )
@@ -52,3 +53,24 @@ def test_calculate_random_seeds_existing(seed_count):
     assert len(seeds) == len(set(seeds))
     assert len(seeds + existing) == len(set(seeds + existing))
     assert len(seeds) == seed_count
+
+
+def test_keyspace_order():
+    ## divide an integer range of 15 into five bins
+    input_draws, random_seeds, foos = [list(range(i, i + 3)) for i in range(0, 9, 3)]
+    branches = [{"foo": foo} for foo in foos]
+    # Define the correct ordering of the keyspace:
+    ordered_keys = [
+        (input_draw, random_seed, branch)
+        # Iterate over branches, then seeds, then draws
+        for input_draw in input_draws
+        for random_seed in random_seeds
+        for branch in branches
+    ]
+
+    full_keyspace = Keyspace(
+        branches=branches, keyspace={"input_draw": input_draws, "random_seed": random_seeds}
+    )
+
+    for test_key, reference_key in zip(full_keyspace, ordered_keys):
+        assert test_key == reference_key
