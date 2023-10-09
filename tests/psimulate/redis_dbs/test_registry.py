899ace12e8d24b40a730b3f4cf6b0e0737678c29
Patrick Nast <pnast@uw.edu>
2023-10-09 13:29:38 -0700

    adjust test

--- tests/psimulate/redis_dbs/test_registry.py
+++ tests/psimulate/redis_dbs/test_registry.py
@@ -10,12 +10,18 @@ def test_allocate_jobs(mocker, num_queues, num_jobs):
     mocker.patch("vivarium_cluster_tools.psimulate.redis_dbs.registry.QueueManager")
     queues = [(f"queue_{i}", i) for i in range(num_queues)]
     manager = RegistryManager(queues, 0)
-    # create a list of dummy jobs based on a list of 100 integers
+    # create a list of dummy jobs based on a list of integers
     jobs = [{"job": i} for i in range(num_jobs)]
-    # make a numpy array of all the jobs in every queue
+    # make an array of all the jobs in every queue
     jobs_by_queue = list(manager.allocate_jobs(jobs))
-    test_jobs = []
-    # Test that queues are monotonically increasing
-    for queue in jobs_by_queue:
-        for i in range(len(queue) - 1):
-            assert queue[i]["job"] < queue[i + 1]["job"]
+    # Check that each "layer" of jobs 
+    largest_queue_size = max(len(queue) for queue in jobs_by_queue)
+    for i in range(largest_queue_size - 1):
+        this_layer = {queue[i]["job"] for queue in jobs_by_queue if len(queue) > i}
+        next_layer = {queue[i + 1]["job"] for queue in jobs_by_queue if len(queue) > i + 1}
+        assert max(this_layer) < min(next_layer)
+        
+            
+            
+        
+    
