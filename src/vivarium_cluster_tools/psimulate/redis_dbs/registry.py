53354652d08fc7538765b6bc790a78ec5911d03c
Patrick Nast <pnast@uw.edu>
2024-01-04 13:53:13 -0800

    lint

--- src/vivarium_cluster_tools/psimulate/redis_dbs/registry.py
+++ src/vivarium_cluster_tools/psimulate/redis_dbs/registry.py
@@ -244,7 +244,12 @@ class QueueManager:
 
 
 class RegistryManager:
-    def __init__(self, redis_processes: List[Tuple[str, int]], submitted_workers: int, num_already_completed: int):
+    def __init__(
+        self,
+        redis_processes: List[Tuple[str, int]],
+        submitted_workers: int,
+        num_already_completed: int,
+    ):
         self._logger = logger.bind(queue="all")
         self._logger.info("Building registries.")
         self._queues = [
