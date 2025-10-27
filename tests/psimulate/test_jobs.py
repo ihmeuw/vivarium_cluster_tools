55faa5a076d69f4cac8fda1c133bbcb5f9152a5b
Patrick Nast <pnast@uw.edu>
2025-10-27 13:56:20 -0700

    add unit test

---
+++ tests/psimulate/test_jobs.py
@@ -0,0 +1,17 @@
+from vivarium_cluster_tools.psimulate.jobs import JobParameters
+from copy import deepcopy
+
+
+def test_branch_config_immutable():
+    original_branch_config = {"foo": "bar", "input_data": {"spam": "eggs"}}
+    params = JobParameters(
+        model_specification="model_spec.yaml",
+        branch_configuration=deepcopy(original_branch_config),
+        input_draw=0,
+        random_seed=1,
+        results_path="results",
+        backup_configuration={},
+        extras={},
+    )
+    params.sim_config
+    assert params.branch_configuration == original_branch_config
