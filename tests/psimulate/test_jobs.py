ad7a6dd53cc4caa219e22057f68f6abee5e397e6
Patrick Nast <130876799+patricktnast@users.noreply.github.com>
2025-10-27 14:29:04 -0700

    add docstring and comment
    Ensure that the branch_configuration remains unchanged during simulation configuration.

--- tests/psimulate/test_jobs.py
+++ tests/psimulate/test_jobs.py
@@ -3,7 +3,8 @@ from copy import deepcopy
 from vivarium_cluster_tools.psimulate.jobs import JobParameters
 
 
-def test_branch_config_immutable():
+def test_branch_config_immutable() -> None:
+    "Test that the branch_configuration doesn't get mutated in place."
     original_branch_config = {"foo": "bar", "input_data": {"spam": "eggs"}}
     params = JobParameters(
         model_specification="model_spec.yaml",
@@ -14,5 +15,5 @@ def test_branch_config_immutable():
         backup_configuration={},
         extras={},
     )
-    params.sim_config
+    params.sim_config # This was previously causing branch_config to change
     assert params.branch_configuration == original_branch_config
