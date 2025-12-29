36fb8f3533de29f1135f3cb25d509321fdf0ee16
Patrick Nast <pnast@uw.edu>
2025-12-29 11:56:51 -0800

    formatting

--- src/vivarium_cluster_tools/psimulate/results/__init__.py
+++ src/vivarium_cluster_tools/psimulate/results/__init__.py
@@ -8,8 +8,8 @@ Results Management
 from vivarium_cluster_tools.psimulate.results.cli_options import (
     backup_freq,
     with_batch_size,
-    with_output_file_size,
     with_no_batch,
     with_no_cleanup,
+    with_output_file_size,
 )
 from vivarium_cluster_tools.psimulate.results.processing import ChunkMap, write_results_batch
