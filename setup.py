87e11b1cc28940ff66e982ae4aaa35ec2efcdd1e
Jim Albright <albrja@uw.edu>
2026-02-19 10:34:55 -0800

    Add support for python 3.12 and 3.13

--- setup.py
+++ setup.py
@@ -48,7 +48,9 @@ if __name__ == "__main__":
         "dill",
         "redis",
         "rq",
-        "vivarium>=3.0.0",
+        # FIXME: Uncomment once Vivarium 4.0.0 is released
+        # "vivarium>=4.0.0",
+        "vivarium @ git+https://github.com/ihmeuw/vivarium.git@release_candidate/v4.0.0",
         "psutil",
         "layered_config_tree",
     ]
