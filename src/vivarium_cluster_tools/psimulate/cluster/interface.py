d76ead91ed3a58de635fcba8b820a3284ed5127d
Patrick Nast <pnast@uw.edu>
2026-03-03 17:05:27 -0800

    Remove Redis/RQ/DRMAA

--- src/vivarium_cluster_tools/psimulate/cluster/interface.py
+++ src/vivarium_cluster_tools/psimulate/cluster/interface.py
@@ -7,15 +7,10 @@ Cluster Interface
 
 from __future__ import annotations
 
-import atexit
-import os
-import shutil
 from pathlib import Path
-from tempfile import _TemporaryFileWrapper
 from typing import Any, NamedTuple
 
 from vivarium_cluster_tools.psimulate.environment import ENV_VARIABLES
-from vivarium_cluster_tools.utilities import get_drmaa
 
 
 def validate_cluster_environment() -> None:
@@ -106,57 +101,3 @@ class NativeSpecification(NamedTuple):
             return int(m) * 60 + int(s)
         else:
             return int(parts[0])
-
-
-def submit_worker_jobs(
-    num_workers: int,
-    worker_launch_script: _TemporaryFileWrapper[str],
-    cluster_logging_root: Path,
-    native_specification: NativeSpecification,
-) -> None:
-    drmaa = get_drmaa()
-    s = drmaa.Session()
-    s.initialize()
-    jt = s.createJobTemplate()
-    jt.workingDirectory = os.getcwd()
-    jt.remoteCommand = shutil.which("sh")
-    jt.args = [worker_launch_script.name]
-    jt.outputPath = f":{str(cluster_logging_root / '%A.%a.log')}"
-    jt.errorPath = f":{str(cluster_logging_root / '%A.%a.log')}"
-    jt.jobEnvironment = {
-        "LC_ALL": "en_US.UTF-8",
-        "LANG": "en_US.UTF-8",
-    }
-    jt.joinFiles = True
-    jt.nativeSpecification = native_specification.to_cli_args()
-
-    job_ids = s.runBulkJobs(jt, 1, num_workers, 1)
-    array_job_id = job_ids[0].split("_")[0]
-
-    def kill_jobs() -> None:
-        try:
-            s.control(array_job_id, drmaa.JobControlAction.TERMINATE)
-        except Exception as e:
-            # Check if the job already finished - if so, this error is expected
-            try:
-                status = s.jobStatus(array_job_id)
-                if status in (drmaa.JobState.DONE, drmaa.JobState.FAILED):
-                    return  # Job already finished, nothing to do
-            except Exception:
-                # If we can't get status, fall back to string matching
-                pass
-
-            # FIXME: Hack around issue where drmaa.errors sometimes doesn't
-            #        exist.
-            error_msg = str(e)
-            # These errors occur when workers have already shut down on their own,
-            # which isn't actually an error. "Unspecified error" is slurm-drmaa's
-            # poor translation of ESLURM_ALREADY_DONE (errno 2021).
-            expected_errors = [
-                "already completing",
-                "Invalid job",
-            ]
-            if not any(err in error_msg for err in expected_errors):
-                raise
-
-    atexit.register(kill_jobs)
