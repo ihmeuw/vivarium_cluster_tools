1c951b975449eb74c116ca482ce5922a4659cbd4
Patrick Nast <pnast@uw.edu>
2026-03-04 12:00:53 -0800

    format

--- src/vivarium_cluster_tools/psimulate/results/__init__.py
+++ src/vivarium_cluster_tools/psimulate/results/__init__.py
@@ -5,6 +5,4 @@ Results Management
 
 """
 
-from vivarium_cluster_tools.psimulate.results.cli_options import (
-    backup_freq,
-)
+from vivarium_cluster_tools.psimulate.results.cli_options import backup_freq
