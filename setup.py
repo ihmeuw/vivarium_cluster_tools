041d887aa4afb4de8c5da985d45e7fa586be0b0c
Jim Albright <albrja@uw.edu>
2026-05-28 15:25:15 -0700

    Add comment

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ if __name__ == "__main__":
         "psutil",
         "layered_config_tree",
         "pytest-xdist",
+        # FIXME: Hotfix for docutils version coming from jobmon-core install
         "sphinx>=5.3,<9.0.0",
     ]
     # TODO: Move this to the install requires if we move VCT to stash.
